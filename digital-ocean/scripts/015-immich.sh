f0ee3f866c3889ec0ef9bf51e00b8d54cf8d3d2d
Kenny Ward <kenny@futo.org>
2025-08-29 12:55:05 -0500

    added build immich test

--- digital-ocean/scripts/015-immich.sh
+++ digital-ocean/scripts/015-immich.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 
 # Add systemd.unified_cgroup_hierarchy=0 to grub
@@ -116,6 +116,48 @@ chown -R immich:immich /home/immich/.config/
 /bin/su -l -s "/bin/bash" -c 'cd /home/immich ; HOME=/home/immich USER=immich PATH=/usr/bin:/sbin:/usr/sbin:$PATH /opt/immich/init.sh skip-run' immich
 
 echo "immich init done"
+
+### Test the immich install ###
+
+# Reload sysctl.conf to open port 80
+sudo sysctl -p /etc/sysctl.conf
+
+
+/bin/su -l -s "/bin/bash" -c 'cd /home/immich/immich-app ; docker compose up -d' immich
+
+if [[ ! -v IMMICH_TEST_RUN_TIMEOUT ]]; then 
+    # Time to wait for immich to be ready on test.
+    IMMICH_TEST_RUN_TIMEOUT=300
+fi
+
+if [[ ! -v IMMICH_TEST_RUN_ENDPOINT ]]; then 
+    # Endpoint to test the immich install.
+    IMMICH_TEST_RUN_ENDPOINT="https://localhost/api/server/ping"
+fi
+
+if [[ ! -v IMMICH_TEST_RUN_SLEEP_TIME ]]; then 
+    # Time between tests.
+    IMMICH_TEST_RUN_SLEEP_TIME=10
+fi
+
+IMMICH_TEST_RUN_START_TIME=$(date +%s)
+
+IMMICH_TEST_STATUS_CODE=""
+
+
+while [ "$IMMICH_TEST_STATUS_CODE" != "200" ]; do
+    if [ $(($(date +%s) - $IMMICH_TEST_RUN_START_TIME)) -gt $IMMICH_TEST_RUN_TIMEOUT ]; then
+        echo "immich test timed out after $IMMICH_TEST_RUN_TIMEOUT seconds"
+        # Test failed, exit the build as error.
+        exit 1
+    fi
+    sleep $IMMICH_TEST_RUN_SLEEP_TIME
+    echo "Waiting for immich to be ready..."
+    IMMICH_TEST_STATUS_CODE=$(curl -k -s -o /dev/null -w "%{http_code}" $IMMICH_TEST_RUN_ENDPOINT)
+done
+
+echo "immich test successful"
+
 
 # Shut down the immich build.
 
